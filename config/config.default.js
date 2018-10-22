760943a658ebcfb215888cadd76985155d955dd3
lronelove <657828543@qq.com>
2018-10-22 15:57:02 +0800

    当环境不同的时候mysql的配置会切换，所以稍微修改一下了配置

--- config/config.default.js
+++ config/config.default.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = appInfo => {
-  const config = exports = {};
+  const config = {};
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1538206202148_6891';
@@ -23,26 +23,26 @@ module.exports = appInfo => {
     allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
   };
 
-  // 数据库的配置
-  config.mysql = {
-     // 单数据库信息配置
-    client: {
-      // host
-      host: 'localhost',
-      // 端口号
-      port: '3306',
-      // 用户名
-      user: 'root',
-      // 密码
-      password: 'root',
-      // 数据库名
-      database: 'segi',
-    },
-  // 是否加载到 app 上，默认开启
-    app: true,
-  // 是否加载到 agent 上，默认关闭
-    agent: false,
-  };
+  // // 数据库的配置
+  // config.mysql = {
+  //    // 单数据库信息配置
+  //   client: {
+  //     // host
+  //     host: 'localhost',
+  //     // 端口号
+  //     port: '3306',
+  //     // 用户名
+  //     user: 'root',
+  //     // 密码
+  //     password: 'root',
+  //     // 数据库名
+  //     database: 'segi',
+  //   },
+  // // 是否加载到 app 上，默认开启
+  //   app: true,
+  // // 是否加载到 agent 上，默认关闭
+  //   agent: false,
+  // };
 
   config.bodyParser = {
     jsonLimit: '10mb',
--- config/config.local.js
+++ config/config.local.js
@@ -1,1 +1,31 @@
 // 本地开发环境的配置
+'use strict';
+
+module.exports = appInfo => {
+    const config = {};
+
+    // 数据库的配置
+    config.mysql = {
+       // 单数据库信息配置
+      client: {
+        // host
+        host: 'localhost',
+        // 端口号
+        port: '3306',
+        // 用户名
+        user: 'root',
+        // 密码
+        password: 'root',
+        // 数据库名
+        database: 'segi',
+      },
+    // 是否加载到 app 上，默认开启
+      app: true,
+    // 是否加载到 agent 上，默认关闭
+      agent: false,
+    };
+
+    return config;
+};
+
+
