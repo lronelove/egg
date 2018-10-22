79fa31d9ed5150da0c93c49edc458ecf5d18acb7
lronelove <657828543@qq.com>
2018-10-22 15:50:22 +0800

    没啥

--- config/config.default.js
+++ config/config.default.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = appInfo => {
-  const config = exports = {};
+  const config = {};
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1538206202148_6891';
